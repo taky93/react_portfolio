e353ff9ed9173912448fc3a71600379aa2c99eab
taky93 <takybre93@gmail.com>
2023-06-25 21:37:25 +0200

    api

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,10 +5,10 @@ function Footer() {
     <div>
       <div className="sm:text-4xl mt-20 flex bg-violet-950 text-white font-bold text-2xl">
         <section>
-          <p className="sm:ml-20 ml-5 py-5 sm:py-10">fsfsfsf</p>
-          <p className="sm:ml-20 ml-5 py-5 sm:py-10">fsfsfsf</p>
-          <p className="sm:ml-20 ml-5 py-5 sm:py-10">fsfsfsf</p>
-          <p className="sm:ml-20 ml-5 py-5 sm:py-10">fsfsfsf</p>
+          <p className="sm:ml-20 ml-5 py-5 sm:py-10">Name:</p>
+          <p className="sm:ml-20 ml-5 py-5 sm:py-10">Number:</p>
+          <p className="sm:ml-20 ml-5 py-5 sm:py-10">Email:</p>
+          <p className="sm:ml-20 ml-5 py-5 sm:py-10">Address:</p>
         </section>
       </div>
     </div>
